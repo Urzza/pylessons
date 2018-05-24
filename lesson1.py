5200533cd0801a084d635af8ee2bac4e6988161d
mc.asdfgf <mc.asdfgf@protonmail.com>
2018-05-25 02:41:30 +0700

    Revert "kawabanga"
    This reverts commit fe8af4ac9afb9a2470094aa769bd11c4bf7e5f4c.

--- lesson1.py
+++
@@ -1,9 +0,0 @@
-from classes.db_class import Collection
-
-collection = Collection("users")
-
-print(collection.find())
-
-collection.insert({"name":"Arnold", "age":33, "counry":"Argentina"})
-
-print(collection.find())
